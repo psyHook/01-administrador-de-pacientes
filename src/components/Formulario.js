26e00118da5c8dd1051f30b19c69f87ebfb6506e
Juan Galarce <j6alarce@gmail.com>
2021-01-14 15:07:59 -0300

    Validando los campos del formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -10,6 +10,8 @@ const Formulario = () => {
     sintomas: "",
   });
 
+  const [error, setError] = useState(false);
+
   // Funcion que se ejecuta cada que el usuario escribe en un input
   const actualizarState = (e) => {
     setCita({
@@ -21,11 +23,31 @@ const Formulario = () => {
   // Extraer los valores
   const { mascota, propietario, fecha, hora, sintomas } = cita;
 
+  // Cuando el usuario presiona agregar cita
+  const submitCita = (e) => {
+    e.preventDefault();
+
+    // Validar la cita
+    if (
+      mascota.trim() === "" ||
+      propietario.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintomas.trim() === ""
+    ) {
+      setError(true)
+      return;
+    }
+    // Asignar un ID
+  };
+
   return (
     <Fragment>
       <h2>Crear Cita</h2>
-
-      <form>
+      {
+        error ? <p className="alerta-error">Todos los campos son obligatorios</p> : null
+      }
+      <form onSubmit={submitCita}>
         <label>Nombre Mascota</label>
         <input
           type="text"
