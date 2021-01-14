cd68b852c79ce74e2af40748badd6c4935dd7e30
Juan Galarce <j6alarce@gmail.com>
2021-01-14 13:58:09 -0300

    Creando state de las citas

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,19 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Formulario = () => {
+  // Crear state de citas con un obj y sus propiedades
+  const [citas, setCitas] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    horas: "",
+    sintomas: "",
+  });
+
+  const actualizarState = () => {
+    console.log("Escribiendo");
+  };
+
   return (
     <Fragment>
       <h2>Crear Cita</h2>
@@ -12,6 +25,7 @@ const Formulario = () => {
           name="mascota"
           className="u-full-width"
           placeholder="Nombre de la mascota"
+          onChange={actualizarState}
         />
         <label>Nombre Dueño</label>
         <input
@@ -19,13 +33,28 @@ const Formulario = () => {
           name="propietario"
           className="u-full-width"
           placeholder="Nombre del dueño"
+          onChange={actualizarState}
         />
         <label>Fecha</label>
-        <input type="date" name="fecha" className="u-full-width" />
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+        />
         <label>Hora</label>
-        <input type="time" name="hora" className="u-full-width" />
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+        />
         <label>Sintomas</label>
-        <textarea className="u-full-width" name="sintomas"></textarea>
+        <textarea
+          className="u-full-width"
+          name="sintomas"
+          onChange={actualizarState}
+        ></textarea>
         <button type="submit" className="u-full-width button-primary">
           Agregar Cita
         </button>
