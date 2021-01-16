45a9dd2c4ecec67f1cf3974164756b955103e7a0
Juan Galarce <j6alarce@gmail.com>
2021-01-15 23:34:54 -0300

    Guardando y eliminando las citas de localstorage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
 import Cita from "./components/Cita";
 
 function App() {
+  // Citas en localstorage
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+
   // Arreglo de citas
-  const [citas, setCitas] = useState([]);
+  const [citas, setCitas] = useState(citasIniciales);
+
+  // Use Effect para realizar operaciones cuando el state cambia
+  useEffect(() => {
+    if (citasIniciales) {
+      localStorage.setItem("citas", JSON.stringify(citas));
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas,citasIniciales]);
 
   // Funcion que tome las citas actuales y agrege la nueva
   const crearCita = (cita) => {
-    setCitas([
-      ...citas,
-      cita
-    ]);
+    setCitas([...citas, cita]);
   };
 
   // Funcionar que elimina una cita por su id
-  const eliminarCita = id => {
-    const nuevasCitas = citas.filter( cita => cita.id !== id);
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter((cita) => cita.id !== id);
     setCitas(nuevasCitas);
-  }
+  };
+
+  // Mensaje condicional
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus citas";
 
   return (
     <Fragment>
@@ -30,8 +45,8 @@ function App() {
             <Formulario crearCita={crearCita} />
           </div>
           <div className="one-half column">
-            <h2>Administra tus Citas</h2>
-            {citas.map(cita => ( 
+            <h2>{titulo}</h2>
+            {citas.map((cita) => (
               <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
             ))}
           </div>
